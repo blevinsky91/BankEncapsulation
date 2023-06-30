6d03e459d892f28a7c70e5bf573507a0a07108ed
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-30 12:55:24 -0400

    complete

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		private double balance = 0;
+
+		public void Deposit(double mula)
+		{
+			balance += mula;
+		}
+
+		public void GetBalance()
+		{
+			Console.WriteLine($"Great, your balance on this account is {balance}. Have a great day!");
+		}
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            BankAccount acc1 = new BankAccount();
+
+            Console.WriteLine("How much money will you be depositing today?");
+            var input = double.Parse(Console.ReadLine());
+            acc1.Deposit(input);
+            acc1.GetBalance(); 
         }
     }
 }
